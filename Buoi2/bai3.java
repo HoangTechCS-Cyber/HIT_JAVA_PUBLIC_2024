c20f22e6cf2db6374cf48e509b58df4d11ca330b
HoangTechCS-Cyber <nguyenhuyhoangqbx5@gmail.com>
2024-07-28 09:22:23 +0700

    up

--- Buoi2/bai3.java
+++ Buoi2/bai3.java
@@ -1,13 +1,42 @@
-package BTVNB2;
 import java.util.Scanner;
-import java.math.*;
+
 public class bai3 {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.print("n= ");
-        int n = sc.nextInt();
-        int a[][] = new int[n][n];
+        Scanner s = new Scanner(System.in);
+        int n;
+        int t=1;
+        n = s.nextInt();
+        int arr[][] = new int[n][n];
 
+        for(int a=n; a>0; a=a-1 )
+        {
+            for(int j = n-a; j<a; ++j)
+            {
+                arr[n-a][j]=t;
+                t=t+1;
+            }
+            for(int i = n-a+1; i<a; ++i)
+            {
+                arr[i][a-1]=t;
+                t=t+1;
+            }
+            for(int j = a-2; j>=n-a; --j)
+            {
+                arr[a-1][j]=t;
+                t=t+1;
+            }
+            for(int i = a-2; i >= n-a+1; --i)
+            {
+                arr[i][n-a]=t;
+                t=t+1;
+            }
+        }
+        for(int i = 0; i<n; ++i)
+        {for(int j = 0; j<n; ++j)
+        {
+            System.out.print(arr[i][j] + " ");
+        }
+            System.out.println("\n");
+        }
     }
-
 }
